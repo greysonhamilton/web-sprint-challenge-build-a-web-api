330af24e55c0128575a354b971d8be78047d7d9a
Greyson <greyson.r.hamilton@gmail.com>
2021-02-21 13:24:13 -0500

    action and projects routing

--- api/middleware/index.js
+++ api/middleware/index.js
@@ -1,4 +1,6 @@
 const actDb = require("../actions/actions-model");
+const projDb = require("../projects/projects-model");
+const projRouter = require("../projects/projects-router");
 
 const checkActD = () => (req, res, next) => {
 
@@ -26,9 +28,33 @@ const checkActId = () => (req, res, next) => {
         })
 };
 
+const checkProjId = () => (req, res, next) => {
+
+    projDb.get(req.params.id)
+        .then((project) => {
+            if (project) {
+                next()
+            } else {
+                res.status(404).json({
+                    message: "This project was not found."
+                })
+            }
+        })
+};
+
+const checkProjD = () => (req, res, next) => {
+    if (!req.body.name || !req.body.description) {
+        return res.status(400).json({
+            message: "Missing input required."
+        })
+    }
+};
+
 module.exports = {
 
     checkActD,
-    checkActId
+    checkActId,
+    checkProjD,
+    checkProjId
 
 };
--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -1,1 +1,76 @@
 // Write your "projects" router here!
+const express = require("express");
+const projDb = require("./projects-model");
+const { checkProjId, checkProjD, checkActD } = require("../middleware/index");
+
+const projRouter = express.Router;
+
+projRouter.length("/api/projects", (req, res, next) => {
+
+    projDb.get(project)
+        .then((project) => {
+            res.status(200).json(project)
+        })
+
+        .catch(next)
+
+});
+
+projRouter.get("/api/projects/:id", checkProjId(), (req, res, next) => {
+
+    projDb.get(req.params.id)
+        .then((project) => {
+            res.status(200).json(project)
+        })
+
+        .catch(next)
+
+});
+
+projRouter.post("/api/projects", checkProjD(), (req, res, next) => {
+
+    projDb.insert(req.body)
+        .then((project) => {
+            res.status(201).json(project)
+        })
+
+        .catch(next)
+
+});
+
+projRouter.put("/api/projects/:id", checkProjId(), checkActD(), (req, res, next) => {
+
+    projDb.update(req.params.id, req.body)
+        .then((project) => {
+            res.status(200).json(project)
+        })
+
+        .catch(next)
+
+});
+
+projRouter.delete("/api/projects/:id", checkProjId(), (req, res, next) => {
+
+    projDb.remove(req.params.id)
+        .then((project) => {
+            res.status(200).json({
+                message: "Successfully deleted."
+            })
+        })
+
+        .catch(next)
+
+});
+
+projRouter.get("/api/projects/:id/actions", checkProjId(), (req, res, next) => {
+
+    projDb.getProjectActions(req.params.id)
+        .then((act) => {
+            res.status(200).json(act)
+        })
+
+        .catch(next)
+
+});
+
+module.exports = projRouter;
